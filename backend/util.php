46b52d142aa17af2f1dfda26f888a34dcb530f5e
luis <luiscarvajal2693@gmail.com>
2018-09-29 13:26:28 -0500

    hotfixes: comments backend

--- backend/util.php
+++ backend/util.php
@@ -1,6 +1,6 @@
 <?php
-//$mysqli = new mysqli("localhost", "root", "", "requeriments");
-$mysqli = new mysqli("db754482285.db.1and1.com", "dbo754482285", "@Green.123!", "db754482285");
+$mysqli = new mysqli("localhost", "root", "", "requeriments");
+//$mysqli = new mysqli("db754482285.db.1and1.com", "dbo754482285", "@Green.123!", "db754482285");
 
 if($_POST['method']=='getRequeriments'){
     getRequeriments();
@@ -20,6 +20,22 @@ if($_POST['method']=='editRequeriment'){
 
 if($_POST['method']=='login'){
     login();
+}
+
+if($_POST['method']=='getComments'){
+    getComments();
+}
+
+if($_POST['method']=='createComment'){
+    createComment();
+}
+
+if($_POST['method']=='deleteComment'){
+    deleteComment();
+}
+
+if($_POST['method']=='editComment'){
+    editComment();
 }
 
 function getRequeriments(){
@@ -44,18 +60,6 @@ function createRequeriment(){
 }
 
 function editRequeriment(){
-    $query = 'UPDATE requeriments (priority, idRow, team, deadline, feature, description, link, statusRequeriment, approved, orderRequeriment) VALUES ('.
-        $_POST['priority'].', \''.
-        $_POST['idRow'].'\', \''.
-        $_POST['team'].'\', \''.
-        $_POST['deadLine'].'\', \''.
-        $_POST['feature'].'\', \''.
-        $_POST['description'].'\', \''.
-        $_POST['link'].'\', \''.
-        $_POST['statusRequeriment'].'\', \''.
-        $_POST['approved'].'\', '.
-        $_POST['orderRequeriment'].')';
-
     $query = 'UPDATE requeriments SET 
                 priority='.$_POST['priority'].', 
                 idRow=\''.$_POST['idRow'].'\', 
@@ -79,6 +83,53 @@ function deleteRequeriment(){
 
 function login(){
     $query = 'SELECT * FROM users WHERE username = \''.$_POST['username'].'\'';
+    executeQuery($query);
+}
+
+function getComments(){
+    $query = 'SELECT * FROM comments';
+    executeQuery($query);
+}
+
+function createComment(){
+    $query = 'INSERT INTO comments (usernameComment, dateComment, contentComment, 
+                                    emailToNotifyComment, flagComment, usernameAnswer, 
+                                    dateAnswer, contentAnswer, emailToNotifyAnswer, 
+                                    flagAnswer, idRequeriment) VALUES ( \''.
+        $_POST['usernameComment'].'\', \''.
+        $_POST['dateComment'].'\', \''.
+        $_POST['contentComment'].'\', \''.
+        $_POST['emailToNotifyComment'].'\', \''.
+        $_POST['flagComment'].'\', \''.
+        $_POST['usernameAnswer'].'\', \''.
+        $_POST['dateAnswer'].'\', \''.
+        $_POST['contentAnswer'].'\', \''.
+        $_POST['emailToNotifyAnswer'].'\', \''.
+        $_POST['flagAnswer'].'\', '.
+        $_POST['idRequeriment'].')';
+
+    executeQuery($query);
+}
+
+function deleteComment(){
+    $query = 'DELETE FROM comments WHERE id= '.$_POST['id'];
+    executeQuery($query);
+}
+
+function editComment(){
+    $query = 'UPDATE comments SET 
+                usernameComment=\''.$_POST['usernameComment'].'\', 
+                dateComment=\''.$_POST['dateComment'].'\', 
+                contentComment=\''.$_POST['contentComment'].'\', 
+                emailToNotifyComment=\''.$_POST['emailToNotifyComment'].'\', 
+                flagComment=\''.$_POST['flagComment'].'\', 
+                usernameAnswer=\''.$_POST['usernameAnswer'].'\', 
+                dateAnswer=\''.$_POST['dateAnswer'].'\', 
+                contentAnswer=\''.$_POST['contentAnswer'].'\', 
+                emailToNotifyAnswer=\''.$_POST['emailToNotifyAnswer'].'\' 
+                flagAnswer=\''.$_POST['flagAnswer'].'\' 
+                WHERE id='.$_POST['id'];
+
     executeQuery($query);
 }
 
